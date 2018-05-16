ebe0f15537780ba4e567cbb56935e3d17a65d634
chenting31 <625587104@qq.com>
2018-05-16 14:24:25 +0800

    new project

---
+++ microservicecloud-consumer-dept-81/src/main/java/com/xtaro/springcloud/DeptConsumer81_App.java
@@ -0,0 +1,12 @@
+package com.xtaro.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptConsumer81_App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeptConsumer81_App.class, args);
+	}
+}
---
+++ microservicecloud-consumer-dept-82/src/main/java/com/xtaro/springcloud/DeptConsumer82_App.java
@@ -0,0 +1,12 @@
+package com.xtaro.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptConsumer82_App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeptConsumer82_App.class, args);
+	}
+}
